a56e7c65d51ba472286cdc5ec32d6667aac12561
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-12 00:57:31 +0530

    Create backend.tf

---
+++ Q3/backend.tf
@@ -0,0 +1,10 @@
+/*terraform {
+  backend "s3" {
+    bucket         = "aminoacid" # change this
+    key            = "example/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "aminoacid"
+  }
+}
+*/
