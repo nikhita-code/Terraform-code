245ddff2ad6599bdcccbd5f0c80622e0bd63410e
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-12 00:32:48 +0530

    Update main.tf

--- Q1/main.tf
+++ Q1/main.tf
@@ -1,4 +1,18 @@
+provider "aws" {
+    region = "us-east-1"  # Set your desired AWS region
+}
 
+resource "aws_instance" "example" {
+    for_each = var.instance_config
+
+    ami           = each.value.amip  # Specify an appropriate AMI ID Ubuntu
+    instance_type = each.value.instance_typep 
+    key_name = "taerraform-demo-codespace"
+    subnet_id = each.value.subnet_idp
+    iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
+    tags = each.value.tagsp
+
+}
 resource "aws_iam_role" "ec2_role_nikhita" {
   name = "my-ec2-role_nikhita"
 
