306e0bf7d76224c028d2fdf819cea5a7ebd6378a
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-12 00:28:12 +0530

    Update main.tf

--- Q1/main.tf
+++ Q1/main.tf
@@ -1,1 +1,50 @@
 
+resource "aws_iam_role" "ec2_role_nikhita" {
+  name = "my-ec2-role_nikhita"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+
+
+resource "aws_iam_policy" "policy" {
+  name        = "test_policy_nikhita"
+  path        = "/"
+  description = "My test policy nikhita"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "role_policy_attachment_nikhita" {
+  role       = aws_iam_role.ec2_role_nikhita.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "my-ec2-instance-profile_nikhita"
+  role = aws_iam_role.ec2_role_nikhita.name
+}
