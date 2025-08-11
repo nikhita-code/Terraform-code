f88510b67fbb6db15ff048093f51800c50fa62e4
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-12 00:41:12 +0530

    Update main.tf

--- Q2/main.tf
+++ Q2/main.tf
@@ -1,1 +1,10 @@
+provider "aws" {
+    region = "us-east-1"  # Set your desired AWS region
+}
 
+module "ec2_instance" {
+  source = "./modules/ec2_instance"
+  amip = var.amip # replace this
+  instance_typep = var.instance_typep
+  subnet_idp =  var.subnet_idp # replace this
+}
