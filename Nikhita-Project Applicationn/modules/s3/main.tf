e6035e988cde23b8bc6b88f13e1afa2cf61293cd
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-15 01:57:35 +0530

    Update main.tf

--- Nikhita-Project Applicationn/modules/s3/main.tf
+++ Nikhita-Project Applicationn/modules/s3/main.tf
@@ -4,12 +4,15 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_caller_identity" "current" {}
+
 resource "aws_s3_bucket" "this" {
    for_each = toset(var.s3_bucket_names)
-   bucket = var.each
-   tags = merge{
-      var.tags, "Name" = "${each.value}-${var.env}"
-   }
+   bucket = each.value
+    
+   tags = merge(
+      var.tags, { Name = "${each.value}-${var.env}" }
+   )
 }
 
 
