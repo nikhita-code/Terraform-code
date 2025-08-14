bcd883c5049d591112b24453f883957f3c3575ca
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-14 23:29:05 +0530

    Update main.tf

--- Nikhita-Project Applicationn/environment/dev/main.tf
+++ Nikhita-Project Applicationn/environment/dev/main.tf
@@ -1,1 +1,12 @@
+locals = {
+  common_tags = {
+    Environment =  var.env
+    Project = "FTR" 
+  }
+}
 
+modules s3 {
+   source = "../../modules/s3"
+   s3_bucket_names = var.s3_bucket_names
+   common_tags = locals.common_tags
+}
