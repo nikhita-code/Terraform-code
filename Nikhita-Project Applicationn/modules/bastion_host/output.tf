68c424c8dcca24f7ea3f6236fe6f4645ca8f7a23
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-15 23:33:17 +0530

    Create output.tf

---
+++ Nikhita-Project Applicationn/modules/bastion_host/output.tf
@@ -0,0 +1,11 @@
+output "instance_id" {
+   value = { for name, inst in aws_instance.this : name => inst.id }
+}
+
+output "instance_public_ip" {
+   value ={ for name, inst in aws_instance.this : name => inst.public_ip }
+}
+
+output "instance_private_ip" {
+   value = { for name, inst in aws_instance.this : name => inst.private_ip }
+}
