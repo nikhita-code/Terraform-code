0392684fcb719b7d2fd3844121a6765d614f6710
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-15 16:01:05 +0530

    Update output.tf

--- Nikhita-Project Applicationn/modules/vpc/output.tf
+++ Nikhita-Project Applicationn/modules/vpc/output.tf
@@ -1,3 +1,11 @@
 output "vps_id" {
   value = aws_vpc.this.id
 }
+
+output "public_subnet_id" {
+    value = [ for s in aws_subnet.public.this : s.id ] 
+}
+
+output "private_subnet_id" {
+    value = [ for s in aws_subnet.private.this : s.id ]
+}
