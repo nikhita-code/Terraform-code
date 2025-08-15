3894479af487ea583e4470a94cec06bbb8dc06c0
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-15 23:22:21 +0530

    Update variable.tf

--- Nikhita-Project Applicationn/modules/vpc/variable.tf
+++ Nikhita-Project Applicationn/modules/vpc/variable.tf
@@ -1,30 +1,31 @@
+variable "env" {
+    type = string
+    default = "dev"
+}
+
 variable "vpc_cidr" {
     type = string
 }
 
-variable "vpc_tags" {
-    type = map(string)
-    default {}
-}
 
 variable "vpc_id" {
   type = string
 }
 
 variable "vpc_public_cidr" {
-  type = list(string)
+  type = string
 }
 
 variable "vpc_private_cidr" {
-   type = list(string)
+   type = string
 }
 
-varible "public_subnet_id" {
-   type = list(string)
+variable "public_subnet_id" {
+   type = string
 }
 
 variable "private_subnet_id" {
-   type = list(string)
+   type = string
 }
 
 variable "name" {
@@ -33,4 +34,12 @@ variable "name" {
 
 variable "tags" {
   type = map(string)
+}
+
+variable "igw_id" {
+   type = string
+}
+
+variable "nat_gateway_id" {
+   type = string
 }
