2b1844870f3afd2d2d4c4b336238484d7e4df6db
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-15 10:08:10 +0530

    Update main.tf

--- Nikhita-Project Applicationn/environment/dev/main.tf
+++ Nikhita-Project Applicationn/environment/dev/main.tf
@@ -6,10 +6,19 @@ locals {
 } 
 
 
-module s3 {
+module "s3" {
    source = "../../modules/s3"
    env = var.env
    s3_bucket_names = var.s3_bucket_names
    tags = local.common_tags
 
+} 
+
+module "aws_instance" {
+   source = "../../modules/ec2_instance"
+   env = var.env
+   ami = var.ami
+   instance_names = var.instance_names
+   instance_type = ami.instnace_type
+   tags = local.common.tags
 }
