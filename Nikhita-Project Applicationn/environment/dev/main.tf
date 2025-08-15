9a3bdb957b74498a1ca00c5cd4cd2f709dbbe42d
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-15 12:09:13 +0530

    Update main.tf

--- Nikhita-Project Applicationn/environment/dev/main.tf
+++ Nikhita-Project Applicationn/environment/dev/main.tf
@@ -1,24 +1,26 @@
 locals {
   common_tags = {
-    #Environment =  var.env
+    Environment =  var.env
     Project = "FTR" 
   }
 } 
 
 
-module "s3" {
+module s3 {
    source = "../../modules/s3"
    env = var.env
    s3_bucket_names = var.s3_bucket_names
    tags = local.common_tags
 
-} 
+}
+
 
 module "aws_instance" {
    source = "../../modules/ec2_instance"
    env = var.env
    ami = var.ami
+   key_pair_nm = var.key_pair_nm
    instance_names = var.instance_names
-   instance_type = ami.instnace_type
-   tags = local.common.tags
+   instance_type = var.instance_type
+   tags = local.common_tags
 }
