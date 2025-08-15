6b76058d277a99d5d893688291f91ccecafc8d78
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-15 23:34:49 +0530

    Update main.tf

--- Nikhita-Project Applicationn/environment/dev/main.tf
+++ Nikhita-Project Applicationn/environment/dev/main.tf
@@ -5,6 +5,33 @@ locals {
   }
 } 
 
+module "vpc" {
+   source = "../../modules/vpc"
+   vpc_cidr = var.vpc_cidr
+   vpc_public_cidr = var.vpc_public_cidr
+   vpc_private_cidr = var.vpc_private_cidr
+   public_subnet_id = var.public_subnet_id
+   private_subnet_id = var.private_subnet_id
+   vpc_id = var.vpc_id
+   igw_id = var.igw_id
+   nat_gateway_id = var.nat_gateway_id
+   name = var.name
+   tags = local.common_tags
+}
+
+
+module "ec2_instance" {
+   source = "../../modules/ec2_instance"
+   env = var.env
+   ami = var.ami
+   key_pair_nm = var.key_pair_nm
+   instance_names = var.instance_names
+   instance_type = var.instance_type
+   subnet_id = aws_subnet.
+
+   tags = local.common_tags
+}
+
 
 module s3 {
    source = "../../modules/s3"
@@ -14,13 +41,3 @@ module s3 {
 
 }
 
-
-module "aws_instance" {
-   source = "../../modules/ec2_instance"
-   env = var.env
-   ami = var.ami
-   key_pair_nm = var.key_pair_nm
-   instance_names = var.instance_names
-   instance_type = var.instance_type
-   tags = local.common_tags
-}
