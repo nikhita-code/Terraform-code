6c49199c7c868db69a09b85979878b79fb007805
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-15 16:17:40 +0530

    Update main.tf

--- Nikhita-Project Applicationn/modules/vpc/main.tf
+++ Nikhita-Project Applicationn/modules/vpc/main.tf
@@ -1,11 +1,18 @@
-resource "aws_vpc" "this" {
+resource "aws_vpc" "" {
    cidr_block = var.vpc_cidr
    enable_dns_support = true
    enable_dns_hostnames = true
    tags = var.tags
 }
 
-
 resource "aws_subnet" "public" {
-   
+   vpc_id = aws_vpc.
+   cidr_block = 
 }
+
+
+resource "aws_internet_gateway" "public_ig" {
+    vpc_id = var.vpc_id
+    tags = { Name = "public_ig" }
+}
+
