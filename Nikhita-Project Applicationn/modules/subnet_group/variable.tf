923ea0df16c0a6928d5b74a129dabcaad80e8608
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-15 13:12:14 +0530

    Update variable.tf

--- Nikhita-Project Applicationn/modules/subnet_group/variable.tf
+++ Nikhita-Project Applicationn/modules/subnet_group/variable.tf
@@ -1,1 +1,20 @@
 
+variable "vpc_id" {
+  type = string
+}
+
+varible "public_subnet_id" {
+   type = list(string)
+}
+
+variable "private_subnet_id" {
+   type = list(string)
+}
+
+variable "name" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
+}
