749e09c33679a1f1563a6554abafc1e2f0d7ed7e
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-15 10:01:15 +0530

    Update main.tf

--- Nikhita-Project Applicationn/modules/ec2_instance/main.tf
+++ Nikhita-Project Applicationn/modules/ec2_instance/main.tf
@@ -1,1 +1,9 @@
 
+resource "aws_instance" "this" {
+   for_each = toset(var.instance_names)
+   ami = var.ami
+   instance_type = var.instance_type
+   tags = merge{
+       var.tags, { Name = "${each.value}-${each.env}"}
+    } 
+}
