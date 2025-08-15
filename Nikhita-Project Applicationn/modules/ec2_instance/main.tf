162c92f39e2759395e0e6974c59e708fbf66c43a
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-15 23:26:03 +0530

    Update main.tf

--- Nikhita-Project Applicationn/modules/ec2_instance/main.tf
+++ Nikhita-Project Applicationn/modules/ec2_instance/main.tf
@@ -1,11 +1,15 @@
 
-resource "aws_instance" "this" {
-  for_each      = toset(var.instance_names)
-  ami           = var.ami
-  instance_type = var.instance_type
-  key_name      = var.key_pair_nm
-  tags = merge(
+resource "aws_instance" "app_server" {
+   for_each      = toset(var.instance_names)
+   ami           = var.ami
+   instance_type = var.instance_type
+   key_name      = var.key_pair_nm
+   subnet_id =  aws_subnet.public.public_subnet_id
+   security_groups = aws_security_group.sg_rule
+   tags = merge(
       var.tags, { Name = "${each.value}-${var.env}" }
    )
 
 }
+
+
