d1c593ded179d29ef613d43bf38e4346857483bc
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-15 23:31:59 +0530

    Create main.tf

---
+++ Nikhita-Project Applicationn/modules/bastion_host/main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "db_server" {
+   ami           = var.ami
+   instance_type = var.instance_type
+   key_name      = var.key_pair_nm
+   subnet_id =   aws_subnet.public.public_subnet_id
+   security_groups = aws_security_group.sg_rule
+   tags = merge(
+      var.tags, { Name = "${each.value}-${var.env}" }
+   )
+
+}
+
