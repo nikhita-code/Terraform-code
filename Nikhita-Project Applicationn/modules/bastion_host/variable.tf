65dbadcc5c94ffbdab24f3e1ba8b09fa42a841f3
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-15 23:33:42 +0530

    Create variable.tf

---
+++ Nikhita-Project Applicationn/modules/bastion_host/variable.tf
@@ -0,0 +1,24 @@
+
+variable "ami" {
+   type = string
+}
+variable "instance_names" {
+   type = list(string)
+}
+
+variable "instance_type" {
+   type = string
+}
+
+variable "key_pair_nm" {
+    type = string
+}
+
+variable "tags" {
+    type = map(string)
+}
+
+variable "env" {
+    type = string
+    default = "dev"
+}
