c2390eb47b623653b0014311f69db98ed1a2e43f
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-15 10:02:59 +0530

    Update variable.tf

--- Nikhita-Project Applicationn/environment/dev/variable.tf
+++ Nikhita-Project Applicationn/environment/dev/variable.tf
@@ -6,3 +6,22 @@ variable "env" {
   type    = string
   default = "dev"
 }
+
+variable "ami" {
+   type = string
+}
+variable "isntance_names" {
+   type = list(string)
+}
+
+variable "instance_type" {
+   type = string
+}
+
+variable "key_pair_nm" {
+    type = string
+}
+
+variable "tags" {
+    type = map(string)
+}
