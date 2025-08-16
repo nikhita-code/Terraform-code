293bbeef52d81eb9e05009f58da1e1daaf039a8c
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-16 16:50:32 +0530

    Update main.tf

--- Nikhita-Project Applicationn/environment/dev/main.tf
+++ Nikhita-Project Applicationn/environment/dev/main.tf
@@ -14,7 +14,6 @@ module "security_groups" {
    vpc_public_cidr = var.vpc_private_cidr
 }
 
-
 module "vpc" {
    source = "../../module/vpc"
    vpc_cidr = var.vpc_cidr
@@ -29,13 +28,6 @@ module "vpc" {
   # tags = local.common_tags
 }
 
-module "security_groups" {
-   source = "../../module/security_groups"
-   vpc_id  = var.vpc_id
-   bastion_cidr    = var.bastion_cidr
-   vpc_private_cidr = var.vpc_private_cidr
-   vpc_public_cidr = var.vpc_private_cidr
-}
 
 
 module "ec2_instance" {
@@ -45,10 +37,11 @@ module "ec2_instance" {
    key_pair_nm = var.key_pair_nm
    instance_names = var.instance_names
    instance_type = var.instance_type
-   subnet_id = var.public_subnet_id
-   vpc_security_group_ids = var.vpc_security_group_ids
+   subnet_id  = var.public_subnet_id
+   vpc_security_group_ids  = [ var.security_group_ids ]
    iam_instance_profile = var.instance_profile_name
    tags = local.common_tags
+   security_group_ids = [ ]
 }
 
 
@@ -65,4 +58,33 @@ module "iam" {
    role_name = var.role_name
    policy_name = var.policy_name
    instance_profile_name = var.instance_profile_name
+}
+
+module "bastion_host" {
+   source = "../../module/bastion_host"
+   env = var.env
+   ami = var.ami
+   key_pair_nm = var.key_pair_nm
+   instance_names = var.instance_names
+   instance_type = var.instance_type
+   subnet_id  = var.public_subnet_id
+   vpc_security_group_ids  = var.vpc_security_group_ids
+   iam_instance_profile = var.instance_profile_name
+   tags = local.common_tags
+   security_group_ids = [ ]
+}
+
+
+module "rds" {
+  source = "../../module/rds"
+  db_instance_identifier = var.db_instance_identifier
+  db_engine              = var.db_engine
+  db_engine_version      = var.db_engine_version
+  db_instance_class      = var.db_instance_class
+  db_alloct_storage  =  var.db_alloct_storage
+  db_username            = var.db_username
+  db_password            = var.db_password
+  db_subnet_group_name =  var.db_subnet_group_name
+  vpc_security_group_ids = var.vpc_security_group_ids.server_rds_sg_id
+  tags                   = var.tags
 }
