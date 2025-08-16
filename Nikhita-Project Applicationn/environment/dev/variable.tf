002f40bc0d84d182ee6c5d3681a0d157ac00ba36
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-16 16:51:12 +0530

    Update variable.tf

--- Nikhita-Project Applicationn/environment/dev/variable.tf
+++ Nikhita-Project Applicationn/environment/dev/variable.tf
@@ -1,3 +1,4 @@
+#s3 bucket module variables
 variable "s3_bucket_names" {
   type = list(string)
 }
@@ -7,6 +8,7 @@ variable "env" {
   default = "dev"
 }
 
+#ec2_instance module variables
 variable "ami" {
    type = string
 }
@@ -27,13 +29,29 @@ variable "tags" {
     type = map(string)
 }  */
 
+#vpc and sg module variables
+variable "name_sg" {
+   type = string
+   default = "Sg-group"
+}
+
+variable "subnet_id" {
+   type = string
+}
+
+variable "bastion_cidr" {
+   type = string
+}
+
 
 variable "vpc_cidr" {
     type = string
 }
 
+
 variable "vpc_id" {
   type = string
+  default  = null
 }
 
 variable "vpc_public_cidr" {
@@ -46,29 +64,98 @@ variable "vpc_private_cidr" {
 
 variable "public_subnet_id" {
    type = string
+   default  = null
 }
 
 variable "private_subnet_id" {
    type = string
+   default  = null
 }
 
 variable "name" {
   type = string
 }
 
+/*variable "tags" {
+  type = map(string)
+}
+*/
 variable "igw_id" {
    type = string
+   default  = null
 }
 
 variable "nat_gateway_id" {
    type = string
+   default  = null
 }
 
-variable "name_sg" {
-   type = string
-   default = "Sg-group"
+#IAM role variables
+variable "role_name" {
+    type = string
+}
+variable "policy_name" {
+    type = string
+    default     = "my-ec2-policy"
+}
+variable "policy_file" {
+    type = string
+    default  = "policy.json"
+}
+variable "instance_profile_name" {
+    type = string
+    default   = "my-ec2-instance-profile"
 }
 
-variable "subnet_id" {
-   type = string
+#rds_db module variables
+variable "db_instance_identifier" {
+    type = string
+    default = "my-db-instance"
+}
+
+variable "db_engine" {
+    type = string 
+    default = "mysql"
+}
+
+variable "db_engine_version" {
+    type = string
+    default = "8.0.32"
+}
+
+variable "db_instance_class" {
+    type = string
+    default = "db.t3.micro"
+}
+
+variable "db_alloct_storage" {
+   type = number
+   default = 20
+}
+
+variable "db_username" {
+    type = string
+}
+
+variable "db_password" {
+    type = string
+    sensitive = true
+}
+
+variable "db_subnet_group_name" { 
+   type = string 
+}
+
+variable "vpc_security_group_ids" {
+  type = string
+  
+}
+
+variable "tags" {
+  description = "A map of tags to apply to resources"
+  type        = map(string)
+  default = {
+    Name        = "app-server"
+    Environment = "dev"
+  }
 }
