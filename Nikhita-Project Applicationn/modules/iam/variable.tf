397ff3a996e1792647b76a43bde31b52e65ddf86
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-16 16:55:36 +0530

    Create variable.tf

---
+++ Nikhita-Project Applicationn/modules/iam/variable.tf
@@ -0,0 +1,15 @@
+variable "role_name" {
+    type = string
+}
+variable "policy_name" {
+    type = string
+    default     = "my-ec2-policy"
+}
+variable "policy_file" {
+    type = string
+    default  = "policy.json"
+}
+variable "instance_profile_name" {
+    type = string
+    default   = "my-ec2-instance-profile"
+}
