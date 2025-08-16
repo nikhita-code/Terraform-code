f25584aa6ecf31fbb102fb89ab6a7f98fe77df80
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-17 01:44:04 +0530

    Update output.tf

--- Nikhita-Project Applicationn/modules/bastion_host/output.tf
+++ Nikhita-Project Applicationn/modules/bastion_host/output.tf
@@ -1,11 +1,11 @@
 output "instance_id" {
-   value = { for name, inst in aws_instance.db_server : name => inst.id }
+   value = aws_instance.db_server.id
 }
 
 output "instance_public_ip" {
-   value ={ for name, inst in aws_instance.db_server : name => inst.public_ip }
+   value = aws_instance.db_server.public_ip
 }
 
 output "instance_private_ip" {
-   value = { for name, inst in aws_instance.db_server : name => inst.private_ip }
+   value = aws_instance.db_server.private_ip
 }
