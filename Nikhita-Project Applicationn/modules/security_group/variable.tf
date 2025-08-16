b791094ab202cbb2addffba7461f66c7687df761
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-16 17:00:01 +0530

    Update variable.tf

--- Nikhita-Project Applicationn/modules/security_group/variable.tf
+++ Nikhita-Project Applicationn/modules/security_group/variable.tf
@@ -1,5 +1,16 @@
+variable "vpc_id" {
+  type = string
+  default  = null
+}
 
-variable "name_sg" {
+variable "vpc_public_cidr" {
+  type = string
+}
+
+variable "vpc_private_cidr" {
    type = string
-   default = "Sg-group"
+}
+
+variable "bastion_cidr" {
+   type = string
 }
