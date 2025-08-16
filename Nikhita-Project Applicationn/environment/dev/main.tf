2a44a6d855da6e5c4ea2c93d261e4724f83a6166
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-16 13:06:02 +0530

    Update main.tf

--- Nikhita-Project Applicationn/environment/dev/main.tf
+++ Nikhita-Project Applicationn/environment/dev/main.tf
@@ -5,8 +5,18 @@ locals {
   }
 } 
 
+
+module "security_groups" {
+   source = "../../module/security_groups"
+   vpc_id  = var.vpc_id
+   bastion_cidr    = var.bastion_cidr
+   vpc_private_cidr = var.vpc_private_cidr
+   vpc_public_cidr = var.vpc_private_cidr
+}
+
+
 module "vpc" {
-   source = "../../modules/vpc"
+   source = "../../module/vpc"
    vpc_cidr = var.vpc_cidr
    vpc_public_cidr = var.vpc_public_cidr
    vpc_private_cidr = var.vpc_private_cidr
@@ -16,28 +26,43 @@ module "vpc" {
    igw_id = var.igw_id
    nat_gateway_id = var.nat_gateway_id
    name = var.name
-   tags = local.common_tags
+  # tags = local.common_tags
+}
+
+module "security_groups" {
+   source = "../../module/security_groups"
+   vpc_id  = var.vpc_id
+   bastion_cidr    = var.bastion_cidr
+   vpc_private_cidr = var.vpc_private_cidr
+   vpc_public_cidr = var.vpc_private_cidr
 }
 
 
 module "ec2_instance" {
-   source = "../../modules/ec2_instance"
+   source = "../../module/ec2_instance"
    env = var.env
    ami = var.ami
    key_pair_nm = var.key_pair_nm
    instance_names = var.instance_names
    instance_type = var.instance_type
-   subnet_id = aws_subnet.
-
+   subnet_id = var.public_subnet_id
+   vpc_security_group_ids = var.vpc_security_group_ids
+   iam_instance_profile = var.instance_profile_name
    tags = local.common_tags
 }
 
 
-module s3 {
-   source = "../../modules/s3"
+module "s3" {
+   source = "../../module/s3"
    env = var.env
    s3_bucket_names = var.s3_bucket_names
    tags = local.common_tags
 
 }
 
+module "iam" {
+   source = "../../module/iam"
+   role_name = var.role_name
+   policy_name = var.policy_name
+   instance_profile_name = var.instance_profile_name
+}
