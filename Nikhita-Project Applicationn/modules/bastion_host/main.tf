22b9a17b3c8556246d96a9955baec81bf0dc83b2
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-16 16:51:47 +0530

    Update main.tf

--- Nikhita-Project Applicationn/modules/bastion_host/main.tf
+++ Nikhita-Project Applicationn/modules/bastion_host/main.tf
@@ -1,12 +1,11 @@
+
 resource "aws_instance" "db_server" {
    ami           = var.ami
    instance_type = var.instance_type
    key_name      = var.key_pair_nm
    subnet_id =   aws_subnet.public.public_subnet_id
    security_groups = aws_security_group.sg_rule
-   tags = merge(
-      var.tags, { Name = "${each.value}-${var.env}" }
-   )
-
+   tags = var.tags
+   
 }
 
