a2fe352505c16dd79cf239ab2e5939c55e68d91a
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-16 16:59:18 +0530

    Update output.tf

--- Nikhita-Project Applicationn/modules/security_group/output.tf
+++ Nikhita-Project Applicationn/modules/security_group/output.tf
@@ -1,8 +1,12 @@
-
-output "server_sg_id" {
-  value = aws_security_group.sg_rule.name
+output "server_bastion_id" {
+  value = aws_security_group.bastion_sg_rule.id
 }
 
-output "server_sg_id" {
-  value = aws_security_group.rds.name
+output "server_rds_sg_id" {
+  value = aws_security_group.rds.id
 }
+
+/*output "server_appsg_id" {
+  value = aws_security_group.app_sg_rule
+}
+*/
